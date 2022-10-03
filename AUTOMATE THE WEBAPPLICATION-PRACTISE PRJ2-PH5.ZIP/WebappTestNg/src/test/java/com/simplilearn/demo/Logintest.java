672bb90d5e3d7fc608b014b9f5dc1f2dab5c1ae4
umayalan <109449811+umayalan@users.noreply.github.com>
2022-10-03 18:50:30 +0530

    PHASE5-PRACTISE2-AUTOMATE THE WEBAPPLICATION

---
+++ AUTOMATE THE WEBAPPLICATION-PRACTISE PRJ2-PH5.ZIP/WebappTestNg/src/test/java/com/simplilearn/demo/Logintest.java
@@ -0,0 +1,39 @@
+package com.simplilearn.demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Logintest {
+	
+	WebDriver driver;
+	@Test
+	public void facebook() {
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("tomjerry@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("dora@123");
+		driver.findElement(By.name("login")).submit();
+		
+		
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pkathiresan\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+		driver=null;
+	}
+	
+	
+}
---
+++ AUTOMATE THE WEBAPPLICATION-PRACTISE PRJ2-PH5.ZIP/WebappTestNg/src/test/java/com/simplilearn/demo/Registertest.java
@@ -0,0 +1,61 @@
+package com.simplilearn.demo;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Registertest {
+  
+	String driverPath="C:\\Users\\pkathiresan\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe";
+	public WebDriver driver;
+	
+  @Test
+  public void f() {
+	  System.setProperty(
+	            "webdriver.chrome.driver", "C:\\Users\\pkathiresan\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        
+	        driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804944%7Cb%7Cfacebook%20%27%7C&placement=&creative=550525804944&keyword=facebook%20%27&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-327195741349%26loc_physical_ms%3D1007765%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIgKXQyLa3-gIVRSUrCh0rwQpyEAAYASAAEgI41vD_BwE");
+	        
+	        driver.manage().window().maximize();
+	        
+	        driver.findElement(By.name("firstname"))
+	        .sendKeys("tom");
+	        
+	        driver.findElement(By.name("lastname"))
+	        .sendKeys("J");
+	        
+	        driver.findElement(By.name("reg_email__"))
+	            .sendKeys("tomjerry@gmail.com");
+	        
+	        driver.findElement(By.name("reg_email_confirmation__"))
+	        .sendKeys("tomjerry@gmail.com");
+	        
+	        driver.findElement(By.id("password_step_input"))
+	        .sendKeys("dora123");
+	        
+	        driver.findElement(By.id("day"))
+	        .sendKeys("03");
+	        
+	        driver.findElement(By.id("month"))
+	        .sendKeys("feb");
+	  
+	        driver.findElement(By.id("year"))
+	            .sendKeys("2001");
+	        
+	        driver.findElement(By.className("_8esa"))
+	        .click();
+	  
+	        driver.findElement(By.name("websubmit"))
+	            .click();
+  }
+	
+	 @AfterTest
+	  public void afterTest() {
+		 System.out.println("After test is running");
+	  }
+}
