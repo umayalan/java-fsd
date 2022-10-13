c14e025f50a18dec1904136e750ba1196f25ddad
umayalan <109449811+umayalan@users.noreply.github.com>
2022-10-13 21:41:44 +0530

    Add files via upload

---
+++ CapstoneProject-My_Aadhar/springboot-backend/src/main/java/com/simplilearn/demo/model/Employee.java
@@ -0,0 +1,118 @@
+package com.simplilearn.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "dob")
+	private String dob;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "email_id")
+	private String emailId;
+	
+	@Column(name = "mobileno")
+	private String mobileNo;
+	@Column(name = "gender")
+	private String gender;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(String firstName, String lastName, String dob, String address, String emailId, String mobileNo,
+			String gender) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.address = address;
+		this.emailId = emailId;
+		this.mobileNo = mobileNo;
+		this.gender = gender;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
