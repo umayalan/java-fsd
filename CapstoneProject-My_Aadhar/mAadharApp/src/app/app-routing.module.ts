dab5ed440bd0d603b96a427245e1f338e0e4575b
umayalan <109449811+umayalan@users.noreply.github.com>
2022-10-13 21:39:17 +0530

    Add files via upload

---
+++ CapstoneProject-My_Aadhar/mAadharApp/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'employees', component: EmployeeListComponent},
+  {path: 'create-employee', component: CreateEmployeeComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'update-employee/:id', component: UpdateEmployeeComponent},
+  {path: 'employee-details/:id', component: EmployeeDetailsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],                                                                                                                                                                                                                                                                                                          
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ CapstoneProject-My_Aadhar/mAadharApp/src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    dob: string;
+    address: string;
+    emailId: string;
+    mobileNo: string;
+    gender: string;
+    }
